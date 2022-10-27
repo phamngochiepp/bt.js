394c63d4f36d41386077beb25d436e1acd4da2b0
phamngochiepp <111079622+phamngochiepp@users.noreply.github.com>
2022-10-27 20:38:39 +0700

    done

---
+++ cau2.js
@@ -0,0 +1,23 @@
+// cau2
+var diem=10;
+if(diem <= 5){
+    console.log(diem+'điểm học lực trung bình');
+}else if(diem <= 6){
+    console.log(diem+'học lực khá');
+}else if(diem <= 8){
+    console.log(diem+'học lực khá gioi');
+}else{
+    console.log(diem+'học lực gioi');
+}
+/*const isEmpty = () => {
+    if (value === null || value === undefined || value === '') {   
+       console.log('rong');
+    }
+    if (Array.isArray(value) && value.length === 0) {
+       return true;
+    }
+    if (typeof value === 'object' && Object.keys(value).length === 0) {
+       return true;
+    }
+    return false;
+ };*/
